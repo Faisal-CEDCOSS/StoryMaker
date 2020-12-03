df9f3ae92d6f58e0122be343536b143355f85118
Faisal13298 <71083554+Faisal13298@users.noreply.github.com>
2020-12-03 11:16:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function function1() 
+{  
+	var v1=document.getElementById("animal1").value;
+	localStorage.setItem("t1",v1);
+	var v2=document.getElementById("animal2").value;
+	localStorage.setItem("t2",v2);
+	var v3=document.getElementById("yesno").value;
+	localStorage.setItem("t3",v3);
+	var v4=document.getElementById("relspeed").value;
+	localStorage.setItem("t4",v4);
+	var v5=document.getElementById("verb").value;
+	localStorage.setItem("t5",v5);
+	var v6=document.getElementById("adj").value;
+	localStorage.setItem("t6",v6);
+	var v7=document.getElementById("quote").value;
+	localStorage.setItem("t7",v7);
+	var v8=document.getElementById("message").value;
+	localStorage.setItem("t8",v8);
+	var v9=document.getElementById("animal3").value;
+	localStorage.setItem("t9",v9);
+	var v10=document.getElementById("num").value;
+	localStorage.setItem("t10",v10);
+	
+	
+
+	return false;
+}
+
